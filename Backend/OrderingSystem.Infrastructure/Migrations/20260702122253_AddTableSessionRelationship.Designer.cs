58a9d0e17caea72a899936661c526326aef16658
Hasan <hasanawata1985@gmail.com>
2026-07-02 15:31:14 +0300

    Fixing dependencies

--- Backend/OrderingSystem.Infrastructure/Migrations/20260702122253_AddTableSessionRelationship.Designer.cs
+++ Backend/OrderingSystem.Infrastructure/Migrations/20260702122253_AddTableSessionRelationship.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OrderingSystem.Infrastructure.Data;
@@ -11,9 +12,11 @@ using OrderingSystem.Infrastructure.Data;
 namespace OrderingSystem.Infrastructure.Migrations
 {
     [DbContext(typeof(OrderingSystemDbContext))]
-    partial class OrderingSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260702122253_AddTableSessionRelationship")]
+    partial class AddTableSessionRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/OrderingSystem.Infrastructure/Migrations/20260702122253_AddTableSessionRelationship.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderingSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableSessionRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Floor",
+                table: "Tables",
+                newName: "FloorNumber");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QrCode",
+                table: "Tables",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FloorNumber",
+                table: "Tables",
+                newName: "Floor");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QrCode",
+                table: "Tables",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
