baf9d2562d5bae98b4c6308794cd03c2a2f8631a
Hasan <hasanawata1985@gmail.com>
2026-07-09 18:56:15 +0300

    Fix: Migration isn't working on production

--- Backend/OrderingSystem/Program.cs
+++ Backend/OrderingSystem/Program.cs
@@ -209,6 +209,7 @@ using (var scope = app.Services.CreateScope())
     {
         var logger = services.GetRequiredService<ILogger<Program>>();
         logger.LogError(ex, "An error occurred while migrating the database.");
+        throw;
     }
 }
 app.Run();
